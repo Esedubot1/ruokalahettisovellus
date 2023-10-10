472dd593b86b2ff29ace70c8da1972ec0918021a
Slava <shukov.sl@gmail.com>
2023-10-10 13:52:21 +0300

    IT WORKSSSS

--- frontend/asiakas/script.js
+++ frontend/asiakas/script.js
@@ -2,13 +2,13 @@ let restaurants = []
 
 
 async function getRestaurants() {
-    fetch("http://localhost:3001/api/restaurants")
+    await fetch("http://localhost:3001/api/restaurants")
         .then(response => response.json())
         .then(data => data.forEach(element => {
             restaurants.push(element)
         }))
 
-    console.log(restaurants)
+    console.log(restaurants[0])
 }
 
 getRestaurants()
