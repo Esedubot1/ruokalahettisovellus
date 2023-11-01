f57931d1b5debc942536687cafc25072f5e20340
Ilmari Kurkela <ilmari.kurkela@esedulainen.fi>
2023-11-01 11:26:26 +0200

    k

--- frontend/login/loginscript.js
+++ frontend/login/loginscript.js
@@ -17,51 +17,29 @@ async function submit(){
       body: JSON.stringify({username: inputUser, password: inputPassword})
     })
 
-    if(res.status === 200 && mode === 'user') {
-      window.localStorage.setItem('loggedUser', JSON.stringify(res))
-      window.location.href = 'asiakas.html'
-    }
-    if(res.status === 200 && mode === 'restaurant') {
-      window.localStorage.setItem('loggedRestaurant', JSON.stringify(res))
-      window.location.href = 'ravintola.html'
-    }
-    if(res.status === 200 && mode === 'deliverer') {
-      window.localStorage.setItem('loggedDeliverer', JSON.stringify(res))
-      window.location.href = 'kuski.html'
-    }
-    if(res.status !== 200) {
+    if (res.status === 200) {
+      switch (mode) {
+        case 'user':
+          window.localStorage.setItem('loggedUser', JSON.stringify(res))
+          window.location.href = 'asiakas.html'
+          break
+        case 'restaurant':
+          window.localStorage.setItem('loggedRestaurant', JSON.stringify(res))
+          window.location.href = 'ravintola.html'
+          break
+        case 'deliverer':
+          window.localStorage.setItem('loggedDeliverer', JSON.stringify(res))
+          window.location.href = 'kuski.html'
+          break
+      }
+    } else {
       errorText.style.display = "block"
       setTimeout(() => {
         errorText.style.display = "none"
       }, 2000)
     }
-
-    /* try {
-        let user = await fetch(`http://localhost:3001/api/login/${mode}`, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({username: inputUser, password: inputPassword})
-        })
-
-        window.localStorage.setItem("xd", JSON.stringify(user))
-    } catch(error) {
-        console.log(error.message)
-        errorText.style.display = "block"
-        setTimeout(() => {
-            errorText.style.display = "none"
-        }, 2000)
-    } */
-    
-    /* if (inputUser == userUser && inputPassword == userPassword) {
-        location.href = "asiakas.html"
-    } else if (inputUser == restaurantUser && inputPassword == restaurantPassword) {
-        location.href = "ravintola.html"
-    } else if (inputUser == driverUser && inputPassword == driverPassword) {
-        location.href = "kuski.html"
-    } else {
-        errorText.style.display = "block"
-        setTimeout(() => {
-            errorText.style.display = "none"
-        }, 2000)
-    } */
 }
+
+document.querySelector('#userButton').onclick = (() => mode = 'user')
+document.querySelector('#restaurantButton').onclick = (() => mode = 'restaurant')
+document.querySelector('#delivererButton').onclick = (() => mode = 'deliverer')
