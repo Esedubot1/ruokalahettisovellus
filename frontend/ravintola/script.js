43654f864bfe96c0204eee01e5fd0b3c5de556d3
Slava <shukov.sl@gmail.com>
2023-11-07 09:28:27 +0200

    orders

--- frontend/ravintola/script.js
+++ frontend/ravintola/script.js
@@ -3,4 +3,16 @@
  const restaurant = JSON.parse(loggedRestaurantJSON)
  const token = restaurant.token
 
- 
+let orders = []
+
+ async function getRestaurantOrders() {
+    orders = []
+    await fetch(`http://localhost:3001/api/orders/from/thisrestaurant`, {headers: {"Authorization": "Bearer " + token}})
+        .then(response => response.json())
+        .then(data => data.forEach(element => {
+            orders.push(element)
+        }))
+    console.log(orders)
+
+    // Pitäisi tehdä niin, että kenttään 
+ }
