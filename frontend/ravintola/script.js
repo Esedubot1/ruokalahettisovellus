7ea6e7165a253fdffb28d4be77e65bcadc875f47
Slava <shukov.sl@gmail.com>
2023-11-13 14:01:49 +0200

    guide

--- frontend/ravintola/script.js
+++ frontend/ravintola/script.js
@@ -5,7 +5,7 @@
 
 let orders = []
 
- async function getRestaurantOrders() {
+async function getRestaurantOrders() {
     orders = []
     await fetch(`http://localhost:3001/api/orders/from/thisrestaurant`, {headers: {"Authorization": "Bearer " + token}})
         .then(response => response.json())
@@ -14,5 +14,25 @@ let orders = []
         }))
     console.log(orders)
 
-    // Pitäisi tehdä niin, että kenttään 
- }
+    createDivs()
+}
+
+async function createDivs(){
+    // Funktio getRestaurantOrders hakee tilaukset datastoragesta ja laittaa ne orders arrayhin. 
+    // Pitäisi tehdä niin, että kenttään restaurant-orders HTML:ässä tulisi näkyviin kaikki tilaukset. Esimerkkiä voi katsoa User sivulta
+    // Jokaisella tilauksella pitäisi olla Tilauksen numero, esim: Order 1, Order 2 etc, tilauksella pitäisi näkyä se ravintola mistä se on tilattu, tilauksen sisältö ja button millä tilauksen voi merkata valmiiksi
+
+    // Suosittelen käyttämään for looppia tätä varten.
+    // Koodi millä saa ravintolan nimen backendistä on seuraava
+
+    // let res = await fetch(`http://localhost:3001/api/restaurants/${orders[i].restaurant}`)
+    // let restaurant = await res.json()
+    // console.log(restaurant.name)
+
+    // Tarvittaessa voi katsoa mallia asiakas folderista, riviltä 227 alaspäin. Siellä oleva koodi on hyvin samanlainen mitä tässä pitää käyttää
+}
+
+async function markAsDone(event){
+    let orderId = event.target.id
+
+}
